616e1966dabdb890168804dc53033cf831c55c2e
vaibhavsingh016 <vaibhav.singh@apollomunichinsurance.com>
2018-07-04 14:00:52 +0530

    basic

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            parseXML();
+        }
+        public static void parseXML(){
+            XmlDocument xd = new XmlDocument();
+            xd.Load("data.xml");
+       
+      XmlNodeList nodelist = xd.SelectNodes("/suite/testcase"); // get all <testcase> nodes
+ 
+      foreach (XmlNode node in nodelist) // for each <testcase> node
+      {
+        
+         
+        Console.WriteLine(node.Attributes.GetNamedItem("id").Value);
+        XmlNode n = node.SelectSingleNode("inputs");
+        Console.WriteLine(n.ChildNodes.Item(0).InnerText);
+      } 
+       
+      
+ 
+    } 
+    }
+}
